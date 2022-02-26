cf88a586d5e00ba8ec8875d1de7c4d6e43ed1475
Junhyeong Park <wnsgudqkr718@gmail.com>
2022-02-26 14:22:09 +0900

    first commit

---
+++ baekjoon/다이나믹 프로그래밍/2225_합분해/2225_합분해/_2225_합분해.py
@@ -0,0 +1,15 @@
+n, k =map(int, input().split())
+
+d = [[0 for j in range(201)] for i in range(201)]
+
+for i in range(1, 201):
+    d[i][1] = 1
+
+for i in range(1, 201):
+    d[1][i] = i
+
+for i in range(2, 201):
+    for j in range(2, 201):
+        d[i][j] = d[i][j - 1] + d[i - 1][j]
+
+print(d[n][k])
